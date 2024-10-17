dea6f5bd9afd71ba61a21ecef7a39452c3ed4cfc
piyanshu31 <piyanshukale2005@gmail.com>
2024-10-17 09:43:24 +0530

    updated

---
+++ p4.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first no: "))
+b =int(input("Enter second no: "))
+
+print("Sum: ", a+b)#sum of two numbers
+print("Sub: ",a-b)#Subtraction of two numbers
+print("Mul: ",a*b)#multiplication of two numbers
+print("Div: ",a/b)#division of two numbers
+print("Rem: ",a%b)#Remainder of two numbers
