0079b4addb72afdcfb5426d2685076ff17a961e9
piyanshu31 <piyanshukale2005@gmail.com>
2024-10-19 13:29:16 +0530

    update

--- p6.py
+++ p6.py
@@ -1,11 +1,22 @@
 #take name from user and print good afternoon and your name
 name = input("Enter your name: ")
 
-print(f"Good afternoon {name}")
+print(f"Good afternoon {name}")#f is needeed in this to print string
 print("Good afternoon "+name)
 
 #make changes in letter
 letter = '''Your <name>
 You are selected!
 <date>'''
+print(letter+"\n")
 print(letter.replace("<name>","Piyanshu").replace("<date>","10 oct 2024"))
+
+#Double space in string
+names="My name  is Piyanshu"
+print(names.find("  "))
+print(names.replace("  "," "))#op as My name is piyanshu
+print(names)#String are immutable which means that you cannot change them be running fuction on them
+
+#formating letter by escape sequence
+letters="This is the python program.\n\tThanks for writing it.."
+print(letters)
