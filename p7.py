4656f33f5e19ae70cf63d9306e157c7357e8ab06
piyanshu31 <piyanshukale2005@gmail.com>
2024-10-19 14:00:26 +0530

    update

---
+++ p7.py
@@ -0,0 +1,21 @@
+#list in python
+student = ["Rohan","25","9th","450/600"]
+print(student)
+print(student[0])
+student[0]="Vishwa"#Unlike string list are mutable
+print(student[0])
+
+#slicing list
+print(student[1:3])
+
+#method in string
+student.append("2nd in boy")
+print(student)#list itself is changed
+
+list = [1,21,221,11,20,122]
+list.insert(4,231)#inserting 231 in 4th postion in list
+print(list)
+list.sort()#sort modified list in ascending order
+print(list)
+list.reverse()#reverse the sorted list
+print(list)
