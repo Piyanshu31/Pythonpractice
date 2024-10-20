7c5d9a5b38ef5770b353e5ed645642cabd206218
piyanshu31 <piyanshukale2005@gmail.com>
2024-10-20 08:56:03 +0530

    update

--- p10.py
+++ p10.py
@@ -3,8 +3,8 @@ power = {
     "halfling vampire":200,
     "vampire":400,
     "Nobal vapire": 600,
-    "Lord of vampire":1000,
-    "King of vapire":1500,
+    "loard of vampire":1000,
+    "King of vampire":1500,
     "Original":3000,
 }
 #It is mutable
@@ -13,3 +13,12 @@ print(power,"\n",type(power))
 print("Power of vampire:",power["vampire"])
 
 #Method of dictionary
+print(power.items(),"\n")
+print(power.keys(),"\n")
+print(power.values(),"\n")
+
+power.update({"Original":4000,"King of vampire":2000})
+print(power)
+
+print(power.get("Monster"))
+print(power.get("loard of vampire"))
