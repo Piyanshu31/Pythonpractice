45c1449b57c1d87c609abe71848945fcf5cb0594
piyanshu31 <piyanshukale2005@gmail.com>
2024-10-20 08:45:19 +0530

    update

---
+++ p10.py
@@ -0,0 +1,15 @@
+#Dictionary in python
+power = {
+    "halfling vampire":200,
+    "vampire":400,
+    "Nobal vapire": 600,
+    "Lord of vampire":1000,
+    "King of vapire":1500,
+    "Original":3000,
+}
+#It is mutable
+print(power,"\n",type(power))
+
+print("Power of vampire:",power["vampire"])
+
+#Method of dictionary
